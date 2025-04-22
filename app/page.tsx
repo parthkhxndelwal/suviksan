bd066c211ed09af8c2eab1089fc5a16db1f140e3
Parth Khandelwal <parthmethi@gmail.com>
2025-04-22 10:42:20 +0530

    commit0

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import HeroText from "@/components/HeroText";
+export default function Home() {
+  return (
+    <>
+    <section id="hero">
+      <HeroText text="Suvikshan"></HeroText>
+    </section>
+    
+    </>
+  );
+}
